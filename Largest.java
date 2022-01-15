189a09ae0de0912aab347c20786fcc7e93fae94f
manav773 <97745534+manav773@users.noreply.github.com>
2022-01-16 03:45:18 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,18 @@
+package First_program_2022;
+
+public class Largest {
+	public static void main(String args[]) {
+		int num1 = 23 , num2 = 45 , num3 = 67;
+		if(num1 > num2 ) {
+			System.out.println(num1);
+		}
+			else if(num2 > num3 )
+			{
+				System.out.println(num2);
+				
+			}
+			else if (num3 > num1) {
+				System.out.println(num3);
+			}
+		}
+	}
